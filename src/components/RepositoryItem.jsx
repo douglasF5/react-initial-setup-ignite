2f51e6fd3b596e2a24b158edc17e4c3bf12bfa2f
Douglas Ferreira <dfaferreira46@gmail.com>
2022-02-08 08:41:51 -0300

    updated component properties

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,9 +3,9 @@ import "../styles/repository-item.scss";
 export function RepositoryItem(props) {
   return (
     <li className="repository-item">
-      <p className="title">{props.repository?.name ?? "default"}</p>
+      <p className="title">{props.repository?.name}</p>
       <p className="description">{props.repository.description}</p>
-      <a className="link" href={props.repository.url}>
+      <a className="link" href={props.repository.html_url}>
         Go to repository
       </a>
     </li>
