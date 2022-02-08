2d01a7d7a69e4996ea0525e4c4249eeeb43baa3f
Douglas Ferreira <dfaferreira46@gmail.com>
2022-02-08 08:43:49 -0300

    added the GitHub api as data source and listed it as cards

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,25 @@
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repository-list.scss";
-
-const repository = {
-  name: "unform2",
-  description: "Forms in React",
-  url: "http://github.com/unform/unform",
-};
+import { useState, useEffect } from "react";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos").then((response) =>
+      response.json().then((data) => setRepositories(data))
+    );
+  }, [repositories]);
+
   return (
     <section className="repository-list">
       <h1>Repositories by Rocketseat</h1>
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
